784312b02893c4e4f88ca6b2aa1d1637a798878f
Andrea Pulcinelli <andreapulcinelligames@gmail.com>
2022-05-29 21:09:44 +0200

    modifiche su item

--- client/src/components/ItemClassifica.vue
+++ client/src/components/ItemClassifica.vue
@@ -1,72 +1,76 @@
 <template>
   <div class="px-4 py-5 my-5 text-center">
-    <div v-if="event">
-      <h1 class="display-5 fw-bold">{{event.name}}</h1>
+    <h1 class="display-5 fw-bold">Classifica</h1>
 
-      <div class="container-md px-4 fs-4" id="legenda">
-        <div class="col-sm-4">
-            Posizione
-          </div>
-          <div class="col-sm-4">
-            Nome
-          </div>
-          <div class="col-sm-4">
-            Scuderia
-          </div>
-          <div class="col-sm-4">
-            Punti
-          </div>
-      </div>
-      <div class="container-md px-4 fs-4" v-for="sessione in event.sessioni" :key="sessione" id="riga">
+      <!-- classifica piloti -->
+      <h2 class="display-5 fw-bold">Classifica piloti</h2>
+      <div class="container-md px-4 fs-4" v-for="pilota in standings.pilotStandings" :key="pilota" id="riga">
 
         <div class="row px-4 py-4">
-          <div class="col-sm-4">
-            {{sessione.nomeSessione}}
+          <div class="col-sm-3">
+            {{pilota.position}}
           </div>
-          <div class="col-sm-4">
-            {{sessione.date}}
+          <div class="col-sm-3">
+            {{pilota.name}}
           </div>
-          <div class="col-sm-4">
-            {{sessione.sessionMyTime}}
+          <div class="col-sm-3">
+            {{pilota.team}}
           </div>
-          <div class="col-sm-4">
-            {{sessione.sessionTrackTime}}
+          <div class="col-sm-3">
+            {{pilota.points}}
           </div>
         </div>
       </div>
-    </div>
-    <div v-else>
-      Loading...
-    </div>
+      
+      <!-- classifica costruttori -->
+      <h2 class="display-5 fw-bold">Classifica costruttori</h2>
+      <div class="container-md px-4 fs-4" v-for="squadra in standings.teamStandings" :key="squadra" id="riga">
+
+        <div class="row px-4 py-4">
+          <div class="col-sm-4">
+            {{squadra.position}}
+          </div>
+          <div class="col-sm-4">
+            {{squadra.name}}
+          </div>
+          <div class="col-sm-4">
+            {{squadra.points}}
+          </div>
+        </div>
+      </div>
+
   </div>
 </template>
 
 <script>
+
 export default {
   props: {
-    event: null,
+    race: null,
   },
   data () {
     return {
+      
     }
-  }
+  },
+  mounted() {
+  },
+  unmounted() {
+  },
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #riga:nth-child(even){
+  #riga:nth-child(odd){
     background-color: white;
     color: black;
   }
   
-  #riga:nth-child(odd){
+  #riga:nth-child(even){
     background-color: #e10600;
     color: white;
   }
-
-  #legenda{
-    background-color: lightgrey;
-    color: black;
-  }
+  
 </style>
--- client/src/components/ItemPiloti.vue
+++ client/src/components/ItemPiloti.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="px-4 py-5 my-5 text-center">
     <h1 class="display-5 fw-bold">Piloti</h1>
-    <div class="container-md px-4 py-4 my-5 fw-bold">
-      <div class="pilotCards">
 
-        <div class="row">
-          <div class="col-sm-6">
-            <div class="card" style="width: 25rem;">
+    <div class="container-md px-4 py-4 my-5 fw-bold" id="pilotCards">
+
+      <div class="row g-3">
+          <div class="col12 col-md-6 col-lg-4">
+            <div class="card">
               <img src="https://www.formula1.com/content/fom-website/en/drivers/charles-leclerc/_jcr_content/image.img.1920.medium.jpg/1646818893219.jpg" class="card-img-top" alt="pilotPortrait.png">
               <div class="card-body">
                 <h5 class="card-title fw-bold">Charles Leclerc</h5> <!-- nomepilota --> 
@@ -15,8 +15,8 @@
             </div>
           </div>
 
-          <div class="col-sm-6">
-            <div class="card" style="width: 25rem;">
+          <div class="col12 col-md-6 col-lg-4">
+            <div class="card">
               <img src="https://www.formula1.com/content/fom-website/en/drivers/charles-leclerc/_jcr_content/image.img.1920.medium.jpg/1646818893219.jpg" class="card-img-top" alt="pilotPortrait.png">
               <div class="card-body">
                 <h5 class="card-title fw-bold">Charles Leclerc</h5> <!-- nomepilota --> 
@@ -25,8 +25,8 @@
             </div>
           </div>
 
-          <div class="col-sm-6">
-            <div class="card" style="width: 25rem;">
+          <div class="col12 col-md-6 col-lg-4">
+            <div class="card">
               <img src="https://www.formula1.com/content/fom-website/en/drivers/charles-leclerc/_jcr_content/image.img.1920.medium.jpg/1646818893219.jpg" class="card-img-top" alt="pilotPortrait.png">
               <div class="card-body">
                 <h5 class="card-title fw-bold">Charles Leclerc</h5> <!-- nomepilota --> 
@@ -34,10 +34,8 @@
               </div>
             </div>
           </div>
-        </div>
-        
-
       </div>
+        
     </div>
 
   </div>
