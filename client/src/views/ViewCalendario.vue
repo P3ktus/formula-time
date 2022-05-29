774f9eafae21ee398476ebd7cdaedab11eee890a
Angelo Iacoella <a.iacoella@2hire.io>
2022-05-29 20:23:26 +0200

    connect calendario to API

--- client/src/views/ViewCalendario.vue
+++ client/src/views/ViewCalendario.vue
@@ -1,22 +1,72 @@
 <template>
-    <div>
-        <ItemCalendario/>
-        <ItemGranpremio/>
-    </div>
+  <div>
+    <ItemCalendario
+      :events="events"
+      :selectedEvent="selectedEventIndex"
+      :onSelect="selectEvent"
+    />
+    <ItemGranpremio :race="selectedEvent" />
+  </div>
 </template>
 
 <script>
-import ItemCalendario from '../components/ItemCalendario.vue'
-import ItemGranpremio from "../components/ItemGranpremio.vue"
+import axios from "axios";
+import moment from "moment-timezone";
+import ItemCalendario from "../components/ItemCalendario.vue";
+import ItemGranpremio from "../components/ItemGranpremio.vue";
 
 export default {
-    components:{
-        ItemCalendario,
-        ItemGranpremio
-    }
-}
+  components: {
+    ItemCalendario,
+    ItemGranpremio,
+  },
+  data() {
+    return {
+      selectedEventIndex: 0,
+      events: null,
+      selectedEvent: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(`http://localhost:8000/calendar`)
+      .then((res) => {
+        this.events = res.data.calendar;
+        this.selectEvent(res.data.currentEventIndex);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  methods: {
+    selectEvent(index) {
+      console.log("AAA");
+      this.selectedEventIndex = index;
+      this.selectedEvent = null;
+      axios
+        .get(`http://localhost:8000/event`, {
+          params: { race_id: this.events[index].race_id },
+        })
+        .then((res) => {
+          this.selectedEvent = {
+            results: res.data.results,
+            event: {
+              ...res.data.event,
+              datetime: moment(res.data.event.datetime),
+              schedule: res.data.event.schedule.map((s) => ({
+                ...s,
+                datetime: moment(s.datetime),
+              })),
+            },
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+          this.error = true;
+        });
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
